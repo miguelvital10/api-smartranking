d128b0779f94f4eaf6890f28e168d06ca8db613e
Miguel Vital <miguelvital05@gmail.com>
2025-01-30 12:57:40 -0300

    api smart ranking

---
+++ src/jogadores/jogador/jogador.interface.ts
@@ -0,0 +1,9 @@
+export interface jogador {
+  readonly _id: string;
+  readonly telefoneCelular: string;
+  readonly email: string;
+  nome: string;
+  ranking: string;
+  posicaoRanking: number;
+  urlFotoJogador: string;
+}   
