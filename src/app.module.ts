fb89b4f5e2f4a0ed24332e8467c84e4cb7f7c5e8
Miguel Vital <miguelvital05@gmail.com>
2025-02-20 09:52:23 -0300

    interface categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:aZoazPdqYzgCVyjC@cluster0.d8wok.mongodb.net/smartranking?retryWrites=true&w=majority'),
-    JogadoresModule],
+    JogadoresModule,
+    CategoriasModule],
   controllers: [],
   providers: [],
 })
