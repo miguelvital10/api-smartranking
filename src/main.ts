e6d73a120d8c07c9e66624303591cc6883560729
Miguel Vital <miguelvital05@gmail.com>
2025-02-27 10:30:58 -0300

    moment timezone

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import * as momentTimezone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter())
+
+  Date.prototype.toJSON = function(): any {
+    return momentTimezone(this)
+    .tz('America/Sao_Paulo')
+    .format('YYYY-MM-DD HH:mm:ss.SSS')
+  }
   await app.listen(process.env.PORT ?? 8083);
 }
 bootstrap();
